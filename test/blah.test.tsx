051067e2616d3b87a414e1446e060333fa195282
MariaLauraElias <eliasmarialaura@gmail.com>
2024-08-14 12:41:43 -0300

    Haciendo unos test

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+// import { Thing } from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    // ReactDOM.render(<Thing />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
+
+// import * as React from 'react';
+// import { createRoot } from 'react-dom/client';
+
+// describe('it', () => {
+//   it('renders without crashing', () => {
+//     const div = document.createElement('div');
+//     const root = createRoot(div);
+//     // root.render(<Thing />);
+//     root.unmount();
+//   });
+// });
